b2c3f1f211ac7e51b05fad70ad563d9155e98299
liwei <liwei@liweideiMac.local>
2018-09-19 11:13:47 +0800

    代码更新到4.2

--- LoterSwift_dev/LoterSwift/AppDelegate.swift
+++ LoterSwift_dev/LoterSwift/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     //MARK: APP进入
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         window = UIWindow()
         window?.backgroundColor = UIColor.white
